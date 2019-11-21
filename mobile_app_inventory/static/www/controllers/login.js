5aabb611bc5aac04d55a1976cb7b726047f39963
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-11-21 13:28:45 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! [MIG] mobile_app_abstract / angular from 11.0 to 12.0 mobile_app_inventory from 8.0 to 12.0

--- mobile_app_inventory/static/www/controllers/login.js
+++ mobile_app_inventory/static/www/controllers/login.js
@@ -5,48 +5,58 @@ angular.module('mobile_app_inventory').controller(
         function ($scope, jsonRpc, $state, $translate, SettingModel, ProductModel) {
 
     $scope.data = {
-        'db': '',
-        'db_list': [],
+        'database': '',
+        'databases': [],
         'login': '',
         'password': '',
     };
 
     $scope.$on('$ionicView.beforeEnter', function() {
         if ($state.current.name === 'logout') {
-            SettingModel.reset_list();
-            ProductModel.reset_list();
+            // SettingModel.reset_list();
+            // ProductModel.reset_list();
             jsonRpc.logout(true);
         }
     });
 
     $scope.init = function () {
         // Set focus
-        angular.element(document.querySelector('#input_login'))[0].focus();
+        // angular.element(document.querySelector('#input_login'))[0].focus();
 
         // Load available databases
-        jsonRpc.get_database_list().then(function(db_list){
-            $scope.data.db_list = db_list;
-            if (db_list.length >= 1) {
-                $scope.data.db = db_list[0];
+        jsonRpc.getDbList().then(function (databases) {
+            $scope.data.databases = databases;
+            if (databases.length >= 1) {
+                $scope.data.database = databases[0];
             }
-        }, function(reason) {
-            $scope.errorMessage = $translate.instant("Unreachable Service");
+        }, function (reason) {
+            $scope.errorMessage = $translate.instant('Unreachable Service');
         });
     };
 
     $scope.submit = function () {
-        jsonRpc.login($scope.data.db, $scope.data.login, $scope.data.password).then(function (user) {
-            jsonRpc.call('mobile.app.inventory', 'check_group', ['stock.group_stock_user']).then(function (res) {
-                if (res){
-                    $scope.errorMessage = "";
-                    $state.go('inventory');
-                }
-                else{
-                    $scope.errorMessage = $translate.instant("Insufficient Acces Right: you should be member of 'Warehouse / user' group.");
+        jsonRpc.login(
+            $scope.data.database,
+            $scope.data.login,
+            $scope.data.password
+        ).then(function (user) {
+            jsonRpc.call(
+                'mobile.app.picking',
+                'check_group',
+                ['stock.group_stock_user']
+            ).then(function (res) {
+                if (res) {
+                    $scope.errorMessage = '';
+                    $state.go('list_picking_type', {});
+                } else {
+                    $scope.errorMessage = $translate.instant(
+                        'Insufficient Acces Right: you should be member of' +
+                        " 'Warehouse / user' group.");
                 }
             });
-        }, function(e) {
-            $scope.errorMessage = $translate.instant("Bad Login / Password");
+        }, function (e) {
+            $scope.errorMessage = $translate.instant('Bad Login / Password');
         });
     };
+
 }]);
